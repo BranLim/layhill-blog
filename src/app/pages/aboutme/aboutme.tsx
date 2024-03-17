f4f9a68a14e786b4b853108b53f2fbf19ccbc802
Brandon Lim <limws.brandon@gmail.com>
2024-03-17 22:29:11 +0800

    Start implementing my own blog/CMS

---
+++ src/app/pages/aboutme/aboutme.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const aboutme = () => {
+  return (
+    <>
+      <h1>About Me</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+};
